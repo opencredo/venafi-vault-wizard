ec9efb21baf5ac3066e0db80bee0510b8c49b97f
Ben Gesoff <ben.gesoff@opencredo.com>
2021-03-11 11:52:20 +0000

    Download plugin from Github and copy to Vault server, then register it
    Downloads from currently hardcoded URL and unzips in-memory before copying to server using SFTP.
    The plugin is in the region of 10-20MB so I figured doing it in memory wouldn't be so bad.

--- vault/errors.go
+++ vault/errors.go
@@ -5,3 +5,4 @@ import "errors"
 var ErrInvalidAddress = errors.New("can't access Vault at address provided")
 var ErrPluginDirNotConfigured = errors.New("plugin_directory not set in Vault config file")
 var ErrReadingVaultPath = errors.New("cannot read Vault path")
+var ErrWritingVaultPath = errors.New("cannot write Vault path")
