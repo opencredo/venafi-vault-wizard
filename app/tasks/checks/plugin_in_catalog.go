2ded9ffdb2bf4ed9354d589a30acdbb1f05d8c26
Ben Gesoff <ben.gesoff@opencredo.com>
2021-04-06 12:39:14 +0100

    Move common steps from plugin install and verify to checks package
    Can't exactly reuse the functions as they do subtly different things, but feels cleaner to have the install and verify halves in the same file.

---
+++ app/tasks/checks/plugin_in_catalog.go
@@ -0,0 +1,39 @@
+package checks
+
+import (
+	"fmt"
+
+	"github.com/opencredo/venafi-vault-wizard/app/reporter"
+	"github.com/opencredo/venafi-vault-wizard/app/vault/api"
+)
+
+func InstallPluginInCatalog(
+	reportSection reporter.Section,
+	vaultClient api.VaultAPIClient,
+	pluginName, sha string,
+) error {
+	check := reportSection.AddCheck("Enabling plugin in Vault plugin catalog...")
+	err := vaultClient.RegisterPlugin(pluginName, pluginName, sha)
+	if err != nil {
+		check.Error(fmt.Sprintf("Error registering plugin in Vault catalog: %s", err))
+		return err
+	}
+
+	check.Success("Successfully registered plugin in Vault plugin catalog")
+	return nil
+}
+
+func VerifyPluginInCatalog(reportSection reporter.Section, vaultClient api.VaultAPIClient, pluginName string) error {
+	check := reportSection.AddCheck("Checking whether plugin is enabled in Vault plugin catalog...")
+	plugin, err := vaultClient.GetPlugin(pluginName)
+	if err != nil {
+		check.Error(fmt.Sprintf("Can't look up plugin in Vault plugin catalog: %s", err))
+		return err
+	}
+	if plugin["command"] != pluginName {
+		check.Error("Plugin enabled, but the currently configured command is incorrect")
+		return fmt.Errorf("wrong plugin command configured")
+	}
+	check.Success("Plugin is enabled in the Vault plugin catalog")
+	return nil
+}
