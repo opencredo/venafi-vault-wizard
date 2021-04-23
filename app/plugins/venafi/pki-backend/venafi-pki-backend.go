b0445d1114ba424cf50d543988f56b20f21c555b
Ben Gesoff <ben.gesoff@opencredo.com>
2021-04-23 10:49:03 +0100

    Implement pki-backend configuration logic

--- app/plugins/venafi/pki-backend/venafi-pki-backend.go
+++ app/plugins/venafi/pki-backend/venafi-pki-backend.go
@@ -1,8 +1,11 @@
 package pki_backend
 
 import (
+	"fmt"
+
 	"github.com/opencredo/venafi-vault-wizard/app/github"
 	"github.com/opencredo/venafi-vault-wizard/app/reporter"
+	"github.com/opencredo/venafi-vault-wizard/app/tasks/checks"
 	"github.com/opencredo/venafi-vault-wizard/app/vault/api"
 )
 
@@ -16,11 +19,74 @@ func (c *VenafiPKIBackendConfig) GetDownloadURL() (string, error) {
 }
 
 func (c *VenafiPKIBackendConfig) Configure(report reporter.Report, vaultClient api.VaultAPIClient) error {
-	// TODO: implement
+	configurePluginSection := report.AddSection("Setting up Venafi PKI backend")
+
+	for _, role := range c.Roles {
+		err := checks.ConfigureVenafiSecret(
+			configurePluginSection,
+			vaultClient,
+			fmt.Sprintf("%s/venafi/%s", c.MountPath, role.Secret.Name),
+			role.Secret.GetAsMap(),
+		)
+		if err != nil {
+			return err
+		}
+
+		err = checks.ConfigureVenafiRole(
+			configurePluginSection,
+			vaultClient,
+			fmt.Sprintf("%s/roles/%s", c.MountPath, role.Name),
+			role.Secret.Name,
+		)
+		if err != nil {
+			return err
+		}
+
+		roleIssuePath := fmt.Sprintf("%s/issue/%s", c.MountPath, role.Name)
+
+		for _, cert := range role.TestCerts {
+			fetchCertSection := report.AddSection(
+				fmt.Sprintf("Requesting test certificate from %s with CN:%s", roleIssuePath, cert.CommonName),
+			)
+			err = checks.RequestVenafiCertificate(
+				fetchCertSection,
+				vaultClient,
+				roleIssuePath,
+				cert.CommonName,
+			)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
 	return nil
 }
 
 func (c *VenafiPKIBackendConfig) Check(report reporter.Report, vaultClient api.VaultAPIClient) error {
-	// TODO: implement
+	configurePluginSection := report.AddSection("Checking Venafi PKI backend")
+
+	for _, role := range c.Roles {
+		err := checks.VerifyVenafiSecret(
+			configurePluginSection,
+			vaultClient,
+			fmt.Sprintf("%s/venafi/%s", c.MountPath, role.Secret.Name),
+			role.Secret.GetAsMap()["zone"].(string),
+		)
+		if err != nil {
+			return err
+		}
+
+		err = checks.VerifyVenafiRole(
+			configurePluginSection,
+			vaultClient,
+			fmt.Sprintf("%s/roles/%s", c.MountPath, role.Name),
+			role.Secret.Name,
+		)
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
