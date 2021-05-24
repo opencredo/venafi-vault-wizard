7440bbb57e1ead3fbc0e99e3a282611810fc9c3f
Ben Gesoff <ben.gesoff@opencredo.com>
2021-05-24 09:55:13 +0100

    Start to flesh out generate-config command
    Uses promptui library to ask the questions and the hclwrite package to turn it into config.
    Still uses some fake data for things like SSH config and only does the Vault part of the config so far.

--- cmd/root.go
+++ cmd/root.go
@@ -11,22 +11,11 @@ import (
 
 func NewRootCommand() *cobra.Command {
 	var configFile string
-	configuration := new(config.Config)
 
 	rootCmd := &cobra.Command{
 		Use:   "vvw",
 		Short: "Venafi Vault Wizard",
 		Long:  "VVW is a wizard to automate the installation and verification of Venafi PKI plugins for HashiCorp Vault.",
-		PersistentPreRunE: func(cmd *cobra.Command, _ []string) error {
-			// Parse provided config file
-			conf, err := config.NewConfigFromFile(configFile)
-			if err != nil {
-				return err
-			}
-
-			configuration = conf
-			return nil
-		},
 	}
 
 	setUpGlobalFlags(rootCmd, &configFile)
@@ -35,12 +24,29 @@ func NewRootCommand() *cobra.Command {
 		Use:   "apply",
 		Short: "Applies desired state as specified in config file",
 		Long:  "Reads the config file and makes necessary changes to Vault server(s) specified to install and configure plugin(s) specified",
-		Run: func(_ *cobra.Command, _ []string) {
+		RunE: func(_ *cobra.Command, _ []string) error {
+			// Parse provided config file
+			configuration, err := config.NewConfigFromFile(configFile)
+			if err != nil {
+				return err
+			}
+
 			commands.Apply(configuration)
+			return nil
+		},
+	}
+
+	generateConfigCmd := &cobra.Command{
+		Use:   "generate-config",
+		Short: "Generates config file based on asking questions",
+		Long:  "Builds up a config file suitable for use with the apply command by asking questions about the Vault setup",
+		Run: func(_ *cobra.Command, _ []string) {
+			commands.GenerateConfig(configFile)
 		},
 	}
 
 	rootCmd.AddCommand(applyCmd)
+	rootCmd.AddCommand(generateConfigCmd)
 
 	return rootCmd
 }
